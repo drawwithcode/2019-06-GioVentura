df2f13f042bf3eaaa467f8656acb5c8778ccedf2
Giorgio Ventura <ventura.giorgio.95@gmail.com>
2019-10-29 18:57:53 +0100

    slider 2 implemented

--- sketch.js
+++ sketch.js
@@ -8,10 +8,10 @@ var slider2;
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 300; i++) {
     let x = random(width);
     let y = random(height);
-    let r = random(10, 40);
+    let r = random(7, 35);
     bubbles[i] = new Bubble(x, y, r);
   }
 
@@ -21,7 +21,7 @@ function setup() {
   button.position((width / 2) - 80, 800);
 
   slider1 = createSlider(2, 130, 17); // it needs a minimum (2) , a maximum (130) and a starting value (17).
-  slider2 = createSlider(5, 40, 10); // RICORDATI DI MOLTIPLICARE PER MENO
+  slider2 = createSlider(0, 80, 3); 
   slider1.position((width / 2) - 72, 700);
   slider2.position((width / 2) - 72, 750);
 
@@ -34,7 +34,7 @@ function changeColor() {
 function draw() {
   background(bgcolor);
   for (let i = 0; i < bubbles.length; i++) {
-    bubbles[i].move();
+    bubbles[i].move(slider2.value());
     bubbles[i].show(slider1.value());
   }
 }
@@ -45,14 +45,14 @@ class Bubble {
     this.y = _y;
     this.r = _r;
   }
-  move() {
-    this.x = this.x + random(-5, 5);
-    this.y = this.y + random(-5, 5);
+  move(_more) {
+    this.x = this.x + random(-_more, _more);
+    this.y = this.y + random(-_more, _more);
   }
 
   show(_extra) {
     stroke(255);
-    strokeWeight(4);
+    strokeWeight(3.4);
     fill(255, 40); // il secondo valore è l'alpha.
     ellipse(this.x, this.y, (this.r * 2) + _extra);
   }
