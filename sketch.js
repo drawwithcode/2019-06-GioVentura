d8ef0fece0060a73a61970956e3992d7d614c2a7
Giorgio Ventura <ventura.giorgio.95@gmail.com>
2019-10-29 19:24:37 +0100

    text reaction

--- sketch.js
+++ sketch.js
@@ -5,8 +5,16 @@ var bubbles = [];
 var slider1;
 var slider2;
 
+var h1;
+var h1b;
+
+var x;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  h1 = createElement('h1', 'Use first slider to control the size.');
+  h1b = createElement('h1', 'Use the second one to control oscillation.');
 
   for (let i = 0; i < 300; i++) {
     let x = random(width);
@@ -15,13 +23,13 @@ function setup() {
     bubbles[i] = new Bubble(x, y, r);
   }
 
-  bgcolor = color(47);
+  bgcolor = color(200);
   button = createButton("Change Background Grey");
   button.mousePressed(changeColor);
   button.position((width / 2) - 80, 800);
 
   slider1 = createSlider(2, 130, 17); // it needs a minimum (2) , a maximum (130) and a starting value (17).
-  slider2 = createSlider(0, 80, 3); 
+  slider2 = createSlider(0, 80, 1);
   slider1.position((width / 2) - 72, 700);
   slider2.position((width / 2) - 72, 750);
 
@@ -37,6 +45,9 @@ function draw() {
     bubbles[i].move(slider2.value());
     bubbles[i].show(slider1.value());
   }
+  x = 100 + random(-slider2.value(), slider2.value());
+  h1.position(x, height / 2 - 20);
+  h1b.position(x, (height / 2) + 20);
 }
 
 class Bubble {
